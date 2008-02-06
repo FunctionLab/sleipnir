1e860e5ac17b0a6882602d2f30068e48034e9e29
chuttenh <chuttenh@localhost>
2008-02-06 11:52:05 -0500

    [svn r260] Move files #2: trimming old stuff

---
+++ tools/MCluster/stdafx.h
@@ -0,0 +1,14 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include <fstream>
+using namespace std;
+
+#include "clusthierarchical.h"
+#include "dat.h"
+#include "measure.h"
+#include "meta.h"
+#include "pcl.h"
+using namespace libBioUtils;
+
+#endif // STDAFX_H
