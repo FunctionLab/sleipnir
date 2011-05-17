443a9d4709f8656fe7c67485baa6fa16cb079138
Casey Greene <csgreene@princeton.edu>
2011-05-17 15:52:58 -0400

    Change documentation for NetworkCombiner to match the program.

--- tools/NetworkCombiner/stdafx.cpp
+++ tools/NetworkCombiner/stdafx.cpp
@@ -22,47 +22,27 @@
 #include "stdafx.h"
 
 /*!
- * \page Combiner Combiner
+ * \page NetworkCombiner NetworkCombiner
  * 
- * Combiner combines things!  Where things include combining PCLs into one large PCL, DAT/DABs into one
- * averaged DAT/DAB, or DAT/DABs into a DAD dataset file.  Multiple PCLs are combined into a single wide
- * PCL by aligning genes' expression vectors and inserting missing values for genes not present in some
- * dataset(s).  DAT/DABs are combined into a single DAT/DAB by averaging pairwise scores in a configurable
- * manner.  DAT/DABs are combined into a DAD by ordering their individual scores as detailed in
- * Sleipnir::CDatasetCompact::Save.
+ * NetworkCombiner can replace the DAT mode of \ref Combiner.  It is faster, but achieves this by making some
+ * important assumptions.  NetworkCombiner uses the mean method of combining and assumes that all gene pairs 
+ * be present in all DABs being combined. Therfore, NetworkCombiner should only be used if these assumptions 
+ * are met.
  * 
  * \section sec_usage Usage
  * 
  * \subsection ssec_usage_basic Basic Usage
  * 
- * \code
- * Combiner -t pcl -o <combined.pcl> <data.pcl>*
- * \endcode
- * 
- * Create a new PCL file \c combined.pcl containing all genes in the microarray PCL files \c data.pcl,
- * with new expression vectors consisting of the concatenation of all data from these input files.  In other
- * words, take the input PCLs, line up each gene's values, smoosh them all together, and plop the result
- * into the output file.
+ * Create a new DAB file from a directory of existing DAB files by calculating the mean for each gene pair
+ * across all of the existing DAB files.
  * 
  * \code
- * Combiner -t dat -o <combined.dab> -n <data.dab>*
+ * NetworkCombiner -d <directory of dabs> -o <combined.dab>
  * \endcode
- * 
- * Create a new DAT/DAB file \c combined.dab in which each gene pair's score is the average of the
- * normalized (by z-scoring) scores from all input DAT/DAB files \c data.dab.  The combination method can be
- * modified using \c -m.
- * 
- * \code
- * Combiner -t dad -o <combined.dad> <data.dab>*
- * \endcode
- * 
- * Create a new DAD file \c combined.dad containing the discretized gene pair scores from all input DAT/DAB
- * files \c data.dab, which must be accompanied by appropriate QUANT files.  This is equivalent to
- * \ref Dab2Dad.
  * 
  * \subsection ssec_usage_detailed Detailed Usage
  * 
- * \include Combiner/Combiner.ggo
+ * \include NetworkCombiner/NetworkCombiner.ggo
  * 
  * <table><tr>
  *	<th>Flag</th>
@@ -70,49 +50,19 @@
  *	<th>Type</th>
  *	<th>Description</th>
  * </tr><tr>
+ * 	<td>-d</td>
  * 	<td>None</td>
- * 	<td>None</td>
- * 	<td>PCL or DAT/DAB files</td>
- * 	<td>Input files to be combined; must all be of an appropriate type for the requested output.</td>
- * </tr><tr>
- *	<td>-t</td>
- *	<td>pcl</td>
- *	<td>pcl, dat, or dad</td>
- *	<td>Type of combination to perform: \c pcl combines PCLs into a PCL, \c dat combines DAT/DABs into a
- *		DAT/DAB, and \c dad combines DAT/DABs into a DAD.</td>
+ * 	<td>DAT/DAB Directory</td>
+ * 	<td>Input directory (must only contain input files as DAT/DAB).</td>
  * </tr><tr>
  *	<td>-m</td>
- *	<td>mean</td>
- *	<td>mean, min, max, gmean, or hmean</td>
- *	<td>Type of DAT/DAB combination to perform when combining pairwise scores.  Options are to calculate the
- *		arithmetic, geometric, or harmonic mean, or to retain only the minimum or maximum value for each
- *		gene pair.</td>
+ *	<td>on</td>
+ *	<td>Flag</td>
+ *	<td>Map gene index among the network dabs to combine (should be used when the gene indices are not identical among network dabs).</td>
  * </tr><tr>
  *	<td>-o</td>
- *	<td>stdout</td>
- *	<td>PCL, DAT/DAB, or DAD file</td>
- *	<td>Output file of the type specified by \c -t.</td>
- * </tr><tr>
- *	<td>-k</td>
- *	<td>2</td>
- *	<td>Integer</td>
- *	<td>Number of columns to skip between the initial ID column and the first experimental (data) column
- *		in the input PCL.</td>
- * </tr><tr>
- *	<td>-p</td>
- *	<td>off</td>
- *	<td>Flag</td>
- *	<td>If given, memory map the input files when possible.  DAT and PCL inputs cannot be memmapped.</td>
- * </tr><tr>
- *	<td>-n</td>
- *	<td>off</td>
- *	<td>Flag</td>
- *	<td>If on, normalize input edges to the range [0,1] before processing.</td>
- * </tr><tr>
- *	<td>-s</td>
- *	<td>0</td>
- *	<td>Integer</td>
- *	<td>If nonzero, process input DAT/DABs in subsets of the requested size as described in
- *		Sleipnir::CDataSubset.</td>
+ *	<td>None</td>
+ *	<td>DAB file</td>
+ *	<td>Output file for combined network.</td>
  * </tr></table>
  */
