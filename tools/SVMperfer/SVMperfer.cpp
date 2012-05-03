410154f1083652fadfac69b8a7f52a26d8e6d4aa
pazpark <pazpark@localhost>
2012-05-03 17:43:38 -0400

    Added functionality in SVMperfer to turn off cross-validation. Useful for small training set size (e.g. 1 example)

--- tools/SVMperfer/SVMperfer.cpp
+++ tools/SVMperfer/SVMperfer.cpp
@@ -158,6 +158,16 @@ int main(int iArgs, char** aszArgs) {
 	} else {
 		SVM.SetPrecisionFraction(sArgs.k_value_arg);
 	}
+
+	
+	if (sArgs.cross_validation_arg < 1){
+	  cerr << "cross_valid is <1. Must be set at least 1" << endl;
+	  return 1;
+	}
+	else if(sArgs.cross_validation_arg < 2){
+	  cerr << "cross_valid is set to 1. No cross validation holdouts will be run." << endl;
+	}
+	
 	SVM.SetTradeoff(sArgs.tradeoff_arg);
 	if (sArgs.slack_flag)
 		SVM.UseSlackRescaling();
@@ -222,23 +232,36 @@ int main(int iArgs, char** aszArgs) {
 		}
 	} else if (sArgs.output_given && sArgs.labels_given) {
 		//do learning and classifying with cross validation
-
-		for (i = 0; i < sArgs.cross_validation_arg; i++) {
-			pTestVector[i].reserve((size_t) vecLabels.size()
-					/ sArgs.cross_validation_arg + sArgs.cross_validation_arg);
-			pTrainVector[i].reserve((size_t) vecLabels.size()
-					/ (sArgs.cross_validation_arg)
-					* (sArgs.cross_validation_arg - 1)
-					+ sArgs.cross_validation_arg);
-			for (j = 0; j < vecLabels.size(); j++) {
-				if (j % sArgs.cross_validation_arg == i) {
-					pTestVector[i].push_back(vecLabels[j]);
-				} else {
-					pTrainVector[i].push_back((vecLabels[j]));
-				}
-			}
+	        if( sArgs.cross_validation_arg > 1){	    
+		  for (i = 0; i < sArgs.cross_validation_arg; i++) {
+		    pTestVector[i].reserve((size_t) vecLabels.size()
+					   / sArgs.cross_validation_arg + sArgs.cross_validation_arg);
+		    pTrainVector[i].reserve((size_t) vecLabels.size()
+					    / (sArgs.cross_validation_arg)
+					    * (sArgs.cross_validation_arg - 1)
+					    + sArgs.cross_validation_arg);
+		    for (j = 0; j < vecLabels.size(); j++) {
+		      if (j % sArgs.cross_validation_arg == i) {
+			pTestVector[i].push_back(vecLabels[j]);
+		      } else {
+			pTrainVector[i].push_back((vecLabels[j]));
+		      }
+		    }
+		  }
 		}
-
+		else{ // if you have less than 2 fold cross, no cross validation is done, all train genes are used and predicted
+		  
+		  // no holdout so train is the same as test gene set
+		  pTestVector[0].reserve((size_t) vecLabels.size() + sArgs.cross_validation_arg);
+		  pTrainVector[0].reserve((size_t) vecLabels.size() + sArgs.cross_validation_arg);
+		  
+		  for (j = 0; j < vecLabels.size(); j++) {
+		    pTestVector[0].push_back(vecLabels[j]);		      
+		    pTrainVector[0].push_back(vecLabels[j]);		    
+		  }
+		}
+		
+		
 		vector<SVMLabel> vec_allUnlabeledLabels;
 		vector<Result> vec_allUnlabeledResults;
 		vector<Result> vec_tmpUnlabeledResults;
@@ -269,11 +292,12 @@ int main(int iArgs, char** aszArgs) {
 			size_t iParams;
 			ofstream ofsm;
 			SVMLight::SAMPLE * ppTrainSample[sArgs.cross_validation_arg];
+			
 			//build all the samples since they are being reused
 			for (i = 0; i < sArgs.cross_validation_arg; i++)
 				ppTrainSample[i] = SVMLight::CSVMPERF::CreateSample(PCL,
 						pTrainVector[i]);
-
+			
 			for (iParams = 0; iParams < PStruct.vecTradeoff.size(); iParams++) {
 				SVM.SetLossFunction(PStruct.vecLoss[iParams]);
 				SVM.SetTradeoff(PStruct.vecTradeoff[iParams]);
@@ -283,7 +307,9 @@ int main(int iArgs, char** aszArgs) {
 				for (i = 0; i < sArgs.cross_validation_arg; i++) {
 					cerr << "Cross Validation Trial " << i << endl;
 					SVM.Learn(*ppTrainSample[i]);
-					cerr << "Learned" << endl;
+					
+					cerr << "Learned" << endl;					
+					
 					tmpAllResults = SVM.Classify(PCL, pTestVector[i]);
 					cerr << "Classified " << tmpAllResults.size()
 							<< " examples" << endl;
