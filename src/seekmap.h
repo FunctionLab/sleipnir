a9d21d6d9050c49b3803c5a15ece689236864523
qzhu <qzhu@princeton.edu>
2013-04-26 01:26:55 -0400

    Added documentation for seekmap and seekplatform

--- src/seekmap.h
+++ src/seekmap.h
@@ -26,25 +26,127 @@
 
 namespace Sleipnir {
 
+/*!
+ * \brief An integer to integer mapping structure
+ *
+ * This map is used to conveniently get a set of available genes in a dataset, and
+ * to quickly check if a given gene is present in the dataset.
+ * Normally, a user needs to create two separate arrays for this purpose.
+ * The first array is the presence vector (0 or 1) that is used to track the presence (1) or
+ * absence (0) of each gene in a given dataset. But this array cannot be used to efficiently
+ * get all available genes in the datasets, because it requires scanning through the entire
+ * presence vector.
+ * The second array contains only available genes in the dataset. But it cannot be used to
+ * efficiently check whether a gene is present, because again it requires a scan-through.
+ *
+ * The solution is to use this int-int mapping structure. It encapsulates the two arrays that
+ * we want: \a forward and \a reverse. As an example, let us consider a simple scenario with a
+ * genome of 5 genes (0, 1, 2, 3, 4), and a dataset \a d contains 3 of 5 genes
+ * (let's say 1, 3, 4).
+ * We want to use this map structure to capture the presence of genes in \a d.
+ * The \a forward array would contain: [-1, 0, -1, 1, 2], where -1 means the gene is absent,
+ * otherwise the value means the \a n-th gene in the array.
+ * The \a reverse array would contain: [1, 3, 4]. (ie., only the available genes).
+ *
+ * These arrays are automatically updated as genes are added to the map.
+ *
+ */
 class CSeekIntIntMap{
 public:
+	/*!
+	 * \brief Constructor
+	 * \param iSize The number of genes in the gene-database
+	 */
 	CSeekIntIntMap(const ushort&);
+
+	/*!
+	 * \brief Constructor
+	 * \param cP The gene presence vector (char type)
+	 * \param bReverse When creating the map, whether or not to follow the reverse logic.
+	 * (eg 1 means absent, 0 means present)
+	 * \remark
+	 * If \c bReverse is true, then this map captures only the absent genes in the dataset.
+	 * By default, \c bReverse is false.
+	 */
 	CSeekIntIntMap(const vector<char>&, const bool=false);
+
+	/*!
+	 * \brief Constructor
+	 * \param cP The gene presence array (char* type)
+	 * \param iSize The size of the gene-presence array
+	 * \param bReverse When creating the map, whether or not to follow the reverse logic.
+	 * (eg 1 means absent, 0 means present)
+	 * \remark
+	 * If \c bReverse is true, then this map captures only the absent genes in the dataset.
+	 * By default, \c bReverse is false.
+	 */
 	CSeekIntIntMap(const char*, const ushort &, const bool=false);
+
+	/*!
+	 * \brief Copy constructor
+	 */
 	CSeekIntIntMap(CSeekIntIntMap*);
+
+	/*!
+	 * \brief Helper function that is used by constructor
+	 */
 	void Initialize(const ushort&);
 
+	/*!
+	 * \brief Destructor
+	 */
 	~CSeekIntIntMap();
+
+	/*!
+	 * \brief Get an element from the \a forward array
+	 * \param i element index
+	 */
 	ushort GetForward(const ushort &) const;
+
+	/*!
+	 * \brief Get an element from the \a reverse array
+	 * \param i element index
+	 */
 	ushort GetReverse(const ushort &) const;
+
+	/*!
+	 * \brief Get the entire \a forward array
+	 */
 	const vector<ushort>& GetAllForward() const;
+
+	/*!
+	 * \brief Get the entire \a reverse array
+	 */
 	const vector<ushort>& GetAllReverse() const;
 
+	/*!
+	 * \brief Add an available gene to the map
+	 */
 	void Add(const ushort&);
+
+	/*!
+	 * \brief Clear the member arrays in the structure
+	 */
 	void Clear();
+
+	/*!
+	 * \brief Reset function
+	 */
 	void Reset(const vector<char>&, const bool=false);
+
+	/*!
+	 * \brief Reset function
+	 */
 	void Reset(const char*, const bool=false);
+
+	/*!
+	 * \brief Get the number of present genes that are currently contained in the map
+	 */
 	ushort GetNumSet() const;
+
+	/*!
+	 * \brief Get the genome size
+	 */
 	ushort GetSize() const;
 
 private:
@@ -55,19 +157,68 @@ private:
 	ushort m_iNumSet;
 };
 
+/*!
+ * \brief A string to integer mapping structure
+ *
+ * Adds <string, integer> pairs into the map.
+ * Supports two major operations:
+ * \c Get(string): returns the corresponding integer
+ * \c Get(integer): returns the corresponding string
+ */
 class CSeekStrIntMap{
 public:
+	/*!
+	 * \brief Constructor
+	 */
 	CSeekStrIntMap();
+	/*!
+	 * \brief Destructor
+	 */
 	~CSeekStrIntMap();
+	/*!
+	 * \brief Clear function
+	 */
 	void Clear();
+	/*!
+	 * \brief Add a pair to the map
+	 * \param s The string
+	 * \param i The integer in unsigned short (ushort)
+	 */
 	void Set(const string&, const ushort&);
+	/*!
+	 * \brief Add all the pairs at once
+	 * \param s A vector of string
+	 * \remarks The corresponding integers are the indices of the strings
+	 * in the vector \c s.
+	 */
 	void SetAll(const vector<string>&);
+	/*!
+	 * \brief Get the corresponding integer for the given string
+	 */
 	ushort Get(const string&) const;
+	/*!
+	 * \brief Get the entire map with key=string, value=integer
+	 */
 	map<string, ushort>& GetMapForward();
+	/*!
+	 * \brief Get the entire map with key=integer, value=string
+	 */
 	map<ushort, string>& GetMapReverse();
+	/*!
+	 * \brief Get the genome size
+	 */
 	ushort GetSize() const;
+	/*!
+	 * \brief Get the corresponding string for the given integer
+	 */
 	string Get(const ushort &) const;
+	/*!
+	 * \brief Retrieve all the strings in the map as a vector
+	 */
 	vector<string> GetAllString() const;
+	/*!
+	 * \brief Retrieve all the integers in the map as a vector
+	 */
 	vector<ushort> GetAllInteger() const;
 private:
 	map<string, ushort> m_mapstrint;
--- src/seekplatform.h
+++ src/seekplatform.h
@@ -25,17 +25,73 @@
 #include "seekbasic.h"
 namespace Sleipnir {
 
+/*!
+ * \brief
+ * A microarray platform that is used by Seek
+ *
+ * Contains the gene correlation average and standard deviation for a given platform
+ *
+ * For each gene \a g in each dataset \a d, we calculate the average correlation of \a g
+ * to all the genes. Then calculate the average of \a g's average correlation across
+ * all the datasets in the platform.
+ * The result is the platform's gene-correlation average, or \c PlatformAvg for short.
+ *
+ * The standard deviation or \c PlatformStdev measures the spread of the gene-correlation
+ * across all datasets of the given platform.
+ *
+ * The purpose of the platform's average and standard deviation are to reduce potential biases
+ * that might be caused by platform specific correlation distributions.
+ */
 class CSeekPlatform{
 public:
+	/*!
+	 * \brief Constructor
+	 */
 	CSeekPlatform();
+	/*!
+	 * \brief Destructor
+	 */
 	~CSeekPlatform();
 
+	/*!
+	 * \brief Initialize the platform
+	 *
+	 * \param numGenes
+	 * The number of genes covered by the platform
+	 *
+	 * \param strPlatformName
+	 * Assign a name to the platform
+	 */
 	void InitializePlatform(const ushort &, const string &);
+
+	/*!
+	 * \brief Set the platform gene-correlation average
+	 */
 	void SetPlatformAvg(const ushort &, const float &);
+
+	/*!
+	 * \brief Set the platform gene-correlation standard deviation
+	 */
 	void SetPlatformStdev(const ushort &, const float &);
+
+	/*!
+	 * \brief Get the platform gene-correlation average
+	 */
 	float GetPlatformAvg(const ushort &) const;
+
+	/*!
+	 * \brief Get the platform gene-correlation standard deviation
+	 */
 	float GetPlatformStdev(const ushort &) const;
+
+	/*!
+	 * \brief Reset
+	 */
 	void ResetPlatform();
+
+	/*!
+	 * Create a copy from a given platform
+	 */
 	void Copy(const CSeekPlatform &);
 
 private:
